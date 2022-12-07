094e48ab7beb2f5a2317f80d813bbbd33afa830c
SashBFa <bastienfajs@gmail.com>
2022-12-07 22:10:09 +0100

    d7 p2

--- 2022/day7/index.js
+++ 2022/day7/index.js
@@ -3,81 +3,90 @@ fs = require('fs');
 
 try {
   // Intitializing the readFileLines with filename
-  const input = fs.readFileSync('input.txt', 'utf8').split('\n');
+  const file = fs.readFileSync('input.txt', 'utf8');
+  const cwd = [];
+  const hierarchy = {};
 
-  const limit = 100000;
-  let document = {
-    name: '/',
-    directory: true,
-    size: 0,
-    children: [],
+  const SIZE_KEY = '_size';
+  const MAX_SIZE = 100_000;
+
+  const addSize = (object, keys, value) => {
+    let copy = object;
+    for (const key of keys) {
+      copy = copy[key] ||= {};
+    }
+    copy[SIZE_KEY] = (copy[SIZE_KEY] || 0) + value;
   };
-  let path = [document];
-  let currentDirectory = path[path.length - 1];
-  let sum = 0;
 
-  for (let line = 0; line < input.length; line++) {
-    if (input[line][0] === '$') {
-      if (input[line][2] === 'l') {
-        for (
-          let i = 1;
-          line + i < input.length && input[line + i][0] !== '$';
-          i++
-        ) {
-          const [left, right] = input[line + i].split(' ');
-          if (left === 'dir') {
-            const dirChildren = {
-              name: right.split('\r')[0],
-              directory: true,
-              size: 0,
-              children: [],
-            };
-            currentDirectory.children.push(dirChildren);
-          } else if (parseInt(left) < limit) {
-            const fileChildren = {
-              name: right.split('\r')[0],
-              directory: false,
-              size: parseInt(left),
-            };
-            currentDirectory.children.push(fileChildren);
-            for (const element of path) {
-              element.size += parseInt(left);
-            }
-          }
+  // Two parties because it's more efficient than bubbling up in real time
+  // Create the hierarchy
+  for (const line of file.split('\n')) {
+    const parts = line.split(' ');
+
+    // Commands
+    if (parts[0] === '$') {
+      // cd
+      if (parts[1] === 'cd') {
+        switch (parts[2]) {
+          case '/':
+            cwd.length = 0;
+            break;
+
+          case '..':
+            cwd.pop();
+            break;
+
+          default:
+            cwd.push(parts[2]);
         }
       }
 
-      if (input[line][2] === 'c') {
-        const [dol, cd, name] = input[line].split(' ');
-        if (name[0].charCodeAt(0) === 47) {
-          //
-        } else if (name[0].charCodeAt(0) === 46) {
-          if (path.length < 1) {
-            path.pop();
-          }
-        } else {
-          let target = currentDirectory.children.filter(
-            (item) => item.name == name.trim()
-          )[0];
+      // Ignore ls
+      continue;
+    }
 
-          let idTarget = currentDirectory.children.indexOf(target);
-          path.push(currentDirectory.children[idTarget]);
-          currentDirectory = currentDirectory.children[idTarget];
-        }
-      }
+    // ls result (ignore directories)
+    if (parts[0] !== 'dir') {
+      addSize(hierarchy, cwd, parseInt(parts[0]));
     }
   }
 
-  const getSum = (doc) => {
-    if (doc.directory) {
-      if (doc.size < limit) sum += doc.size;
-      if (doc.children.length > 0) doc.children.map((child) => getSum(child));
+  // Bubble the sums up
+  const bubbleUp = (hierarchy) => {
+    if (!hierarchy[SIZE_KEY]) {
+      hierarchy[SIZE_KEY] = 0;
     }
+
+    for (const [key, value] of Object.entries(hierarchy)) {
+      if (key === SIZE_KEY) {
+        continue;
+      }
+
+      hierarchy[SIZE_KEY] += bubbleUp(value)[SIZE_KEY] || 0;
+    }
+
+    return hierarchy;
   };
 
-  document.size = 0;
-  getSum(document);
-  console.log(sum);
+  const sum = bubbleUp(hierarchy);
+
+  // Find directories that are under a certain size
+  const calculateSum = (hierarchy) => {
+    let sum = 0;
+
+    for (const value of Object.values(hierarchy)) {
+      if (value[SIZE_KEY] < MAX_SIZE) {
+        sum += value[SIZE_KEY];
+      }
+
+      // Recursion
+      sum += calculateSum(value);
+    }
+
+    return sum;
+  };
+
+  console.log(calculateSum(sum));
 } catch (e) {
   // Printing error
   console.log('Error:', e.stack);
@@ -121,3 +130,134 @@ try {
 // }
 // directory.map((item) => item[1] < limit && (result += item[1]));
 // console.log(directory);
+
+// const limit = 100000;
+// let document = {
+//   name: '/',
+//   directory: true,
+//   size: 0,
+//   children: [],
+// };
+// let path = [document];
+// let currentDirectory = path[path.length - 1];
+// let sum = 0;
+
+// for (let line = 0; line < input.length; line++) {
+//   if (input[line][0] === '$') {
+//     if (input[line][0] === '$' && input[line][2] === 'l') {
+
+//       for (
+//         let i = 1;
+//         line + i < input.length || input[line + i][0] !== '$';
+//         i++
+//       ) {
+//         const [left, right] = input[line + i].split(' ');
+//         if (left === 'dir') {
+//           const dirChildren = {
+//             name: right.split('\r')[0],
+//             directory: true,
+//             size: 0,
+//             children: [],
+//           };
+//           currentDirectory.children.push(dirChildren);
+//         } else if (parseInt(left) < limit) {
+//           const fileChildren = {
+//             name: right.split('\r')[0],
+//             directory: false,
+//             size: parseInt(left),
+//           };
+//           currentDirectory.children.push(fileChildren);
+//           for (const element of path) {
+//             element.size += parseInt(left);
+//           }
+//         }
+//       }
+//     } else if (input[line][2] === 'c') {
+//       const [dol, cd, name] = input[line].split(' ');
+//       if (name[0].charCodeAt(0) === 47) {
+//         //
+//       } else if (name[0].charCodeAt(0) === 46) {
+//         if (path.length > 1) {
+//           path.pop();
+//         }
+//       } else {
+//         let target = currentDirectory.children.filter(
+//           (item) => item.name == name.trim()
+//         )[0];
+
+//         let idTarget = currentDirectory.children.indexOf(target);
+//         path.push(currentDirectory.children[idTarget]);
+//         currentDirectory = currentDirectory.children[idTarget];
+//       }
+//     }
+//   }
+// }
+
+// const getSum = (doc) => {
+//   if (doc.directory) {
+//     if (doc.size < limit) sum += doc.size;
+//     if (doc.children.length > 0) doc.children.map((child) => getSum(child));
+//   }
+// };
+
+// // document.size = 0;
+// // getSum(document);
+// console.log(document);
+
+// const limit = 100000;
+// let document = {
+//   name: '/',
+//   directory: true,
+//   size: 0,
+//   children: [],
+// };
+// let path = [document];
+// let currentDirectory = path[path.length - 1];
+// let sum = 0;
+
+// for (const line of input) {
+//   if (line.startsWith('$')) {
+//     const [dol, cd, name] = line.split(' ');
+//     if (line.startsWith('$ cd ..')) {
+//       path.pop();
+//     } else if (line.startsWith('$ cd') && !line.startsWith('$ cd /')) {
+//       let target = currentDirectory.children.filter(
+//         (item) => item.name == name.trim()
+//       )[0];
+//       let idTarget = currentDirectory.children.indexOf(target);
+
+//       path.push(currentDirectory.children[idTarget]);
+//     }
+//   } else {
+//     const [left, right] = line.split(' ');
+//     if (line.startsWith('dir')) {
+//       const dirChildren = {
+//         name: right.split('\r')[0],
+//         directory: true,
+//         size: 0,
+//         children: [],
+//       };
+//       currentDirectory.children.push(dirChildren);
+//     } else if (parseInt(left) <= limit) {
+//       const fileChildren = {
+//         name: right.split('\r')[0],
+//         directory: false,
+//         size: parseInt(left),
+//       };
+//       currentDirectory.children.push(fileChildren);
+//       for (const element of path) {
+//         element.size += parseInt(left);
+//       }
+//     }
+//   }
+// }
+
+// const getSum = (doc) => {
+//   if (doc.directory) {
+//     if (doc.size <= limit) sum += doc.size;
+//     if (doc.children.length > 0) doc.children.map((child) => getSum(child));
+//   }
+// };
+
+// getSum(document);
+// console.log(sum);
